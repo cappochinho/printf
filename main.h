6da8ce701b6f143df909f7925fbbf28bca07cc7d
cappochinho <65264688+cappochinho@users.noreply.github.com>
2022-07-10 14:36:11 +0000

    Part one of the printf project

--- main.h
+++ main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <unistd.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,5 +8,29 @@
 #include <string.h>
 
 int _printf(const char *format, ...);
+int _putchar(char c);
+void handle_binary(unsigned int n, unsigned int *p);
+int handle_char(va_list arg);
+int handle_int(va_list arg);
+int handle_STR(va_list arg);
+int handle_str(va_list arg);
+int handle_unsigned(va_list arg);
+int handle_unsignedToBinary(va_list arg);
+int handle_oct(va_list arg);
+int handle_unsignedIntToHex(unsigned int num, char _case);
+int handle_hex_base(va_list arg, char _case);
+int handle_hex(va_list arg);
+int handle_HEX(va_list arg);
+
+/**
+ * struct identifierStruct - structure definition of a printTypeStruct
+ * @indentifier: type
+ * @printer: function to print
+ */
+typedef struct identifierStruct
+{
+    char *indentifier;
+    int (*printer)(va_list);
+} identifierStruct;
 
 #endif
